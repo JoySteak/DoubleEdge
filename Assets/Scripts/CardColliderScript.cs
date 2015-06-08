571c03a6f8f523b831ba55635d0d679e3be2e2f3
ksgi15 <kevin.sgi15@gmail.com>
2015-06-08 12:00:34 +0800

    Card - Sprite Prefab Repush

---
+++ Assets/Scripts/CardColliderScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardColliderScript : MonoBehaviour
+{
+	private CardScript m_parentCardScript;
+
+	// Use this for initialization
+	void Start(){
+		m_parentCardScript = gameObject.GetComponentInParent<CardScript>();
+	}
+	
+	// Update is called once per frame
+	void Update(){
+	
+	}
+}
